82dbe4f9b1edc421ad527b9817667d2ed04ea884
Sam Black <sam.black@lubbockisd.org>
2022-11-30 09:44:40 -0600

    Fors and Whiles

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,23 @@ import static java.lang.System.*;
 public class Main {
     public static void main(String[] args) {
         out.println("Hello world!");
+        int value = 0;
 
+        //Loops
+        // 2 families
+        //  - conditional -- while loops
+        //  - fixed iteration -- for loops
+        // examples
+        while (value <= 20){
+            value++;
+            out.println(value);
+
+        }
+        out.println("done");
+
+        for( int index = 30  ; index > 15  ; index -= 4   ){
+            out.println("passthrough");
+        }
 
     }
 }
